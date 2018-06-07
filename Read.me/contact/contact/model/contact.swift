94a68652a0124720b03c41e5a0004daee71a4adc
David Mamou <david_mamou@live.fr>
2018-06-07 17:48:20 +0200

    contact  project

---
+++ Read.me/contact/contact/model/contact.swift
@@ -0,0 +1,16 @@
+//
+//  contact.swift
+//  contact
+//
+//  Created by David Mamou on 6/06/18.
+//  Copyright © 2018 David Mamou. All rights reserved.
+//
+
+import Foundation
+
+
+struct contactStruct{
+    let name: String
+    let familyName: String
+    let number: String
+}
